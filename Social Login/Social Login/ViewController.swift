4018715602d524c46c71cede52efb6c4a464e74f
Tolga Caner <caner.tolga@gmail.com>
2016-07-31 16:39:50 +0300

    initial

---
+++ Social Login/Social Login/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Social Login
+//
+//  Created by Tolga Caner on 31/07/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
