4a20ee3186384b5b534c3319fcd6ac64b58b549b
Tolga Caner <tolga@opera.com>
2016-09-01 14:37:29 +0300

    adapter changes

--- Social Login/Social Login/Adapter.playground/Contents.swift
+++ Social Login/Social Login/Adapter.playground/Contents.swift
@@ -1,45 +1,45 @@
 import Foundation
 
-protocol RemoteControlProtocol {
-    var currentChannel: Int {get set}
-    init(channel:Int)
+//For any class that supports having channels : just for convenience
+protocol Channels {
+    var currentChannel : Int {get set}
+    init(channel: Int)
 }
 
-protocol OldRemoteControlProtocol {
-    //This is an old remote control's model, which has only next and previous channel controls, at the time, it wasn't supposed to change to a specific channel
+//Old remote controls used to support this
+protocol RemoteControlProtocol {
     func nextChannel()
     func prevChannel()
 }
 
+//New remote controls are much more free in zapping
 protocol NewRemoteControlProtocol {
     func switchChannelTo(channel: Int)
 }
 
-//This is the old remote control, which conforms to the OldRemoteControlProtocol.
-class OldRemoteControl : RemoteControlProtocol, OldRemoteControlProtocol {
-    var currentChannel: Int = 0
+
+class RemoteControl : Channels, RemoteControlProtocol {
     
-    required init(channel:Int) {
+    var currentChannel: Int
+    
+    required init(channel: Int) {
         self.currentChannel = channel
     }
     
     func nextChannel() {
-        //target.switchChannelTo(target.currentChannel + 1)
-        currentChannel += 1
+        self.currentChannel += 1
     }
     
     func prevChannel() {
-      //  target.switchChannelTo(target.currentChannel - 1)
-        currentChannel -= 1
+        self.currentChannel -= 1
     }
 }
 
-//Adaptee - The new remote control is so good, it can switch the channel to the number the user wants, there's no need for next and previous. It's suberb! Though it still needs to be compatible with the old remote control's "interface"
-final class RemoteControl : RemoteControlProtocol, NewRemoteControlProtocol {
+class NewRemoteControl : Channels, NewRemoteControlProtocol {
     
     var currentChannel: Int
     
-    init(channel:Int) {
+    required init(channel: Int) {
         self.currentChannel = channel
     }
     
@@ -49,9 +49,11 @@ final class RemoteControl : RemoteControlProtocol, NewRemoteControlProtocol {
 }
 
 // Adapter
-class RemoteControlAdapter: OldRemoteControlProtocol {
-    private var target : RemoteControl!
-    init(_ target:RemoteControl) {
+class RemoteControlAdapter: RemoteControlProtocol {
+    
+    private var target : NewRemoteControl!
+    
+    init(_ target:NewRemoteControl) {
         self.target = target
     }
     
@@ -64,15 +66,15 @@ class RemoteControlAdapter: OldRemoteControlProtocol {
     }
 }
 
-var remoteControl = RemoteControl(channel: 0)
-let remoteControlAdapter = RemoteControlAdapter(remoteControl)
-while remoteControl.currentChannel < 5 {
+let newRemoteControl = NewRemoteControl(channel: 0)
+let remoteControlAdapter = RemoteControlAdapter(newRemoteControl)
+while newRemoteControl.currentChannel < 5 {
     remoteControlAdapter.nextChannel()
 }
-print(remoteControl.currentChannel)
-while remoteControl.currentChannel > 3 {
-    remoteControlAdapter.prevChannel()
+print(newRemoteControl.currentChannel)
+while newRemoteControl.currentChannel > 3 {
+  remoteControlAdapter.prevChannel()
 }
-print(remoteControl.currentChannel)
-remoteControl.switchChannelTo(20)
-print(remoteControl.currentChannel)
+print(newRemoteControl.currentChannel)
+newRemoteControl.switchChannelTo(20)
+print(newRemoteControl.currentChannel)
